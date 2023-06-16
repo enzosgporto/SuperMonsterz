ddf3c45ea00feb6940196b9ec460eef8fd197ed5
enzosgporto <enzosgporto@hotmail.com>
2023-06-16 10:48:59 -0500

    Fixed switching between scenes once the user completes character creation

--- src/main/java/com/example/supermonsterz/ProfileController.java
+++ src/main/java/com/example/supermonsterz/ProfileController.java
@@ -7,13 +7,16 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+
 public class ProfileController {
 
     public Button playButton;
     public ColorPicker colorSelector;
     public TextField nameField;
 
-    public void playAction(ActionEvent actionEvent) {
+    public void playAction(ActionEvent actionEvent) throws IOException {
         SuperMonsterzApplication.Player = new player(nameField.getText(), 100, colorSelector.getValue());
+        SuperMonsterzApplication.setScene("game-view");
     }
 }
