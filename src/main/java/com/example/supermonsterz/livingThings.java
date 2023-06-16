94d7639bd60dbd35b437adf47279479b21e7c859
enzosgporto <enzosgporto@hotmail.com>
2023-06-16 10:33:01 -0500

    Created living things class

--- src/main/java/com/example/supermonsterz/livingThings.java
+++ src/main/java/com/example/supermonsterz/livingThings.java
@@ -1,4 +1,30 @@
 package com.example.supermonsterz;
 
 public class livingThings {
+    final String name;
+    int speed;
+    int health;
+
+    public livingThings(String name, int speed){
+        this.name = name;
+        this.health = health;
+    }
+
+    public livingThings(String name, int speed, int health){
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 }
