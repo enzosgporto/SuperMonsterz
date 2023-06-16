a83b3c8db4bb771610769adf76ed8a471e0a0e2f
enzosgporto <enzosgporto@hotmail.com>
2023-06-16 10:53:15 -0500

    Added visual component to living things class

--- src/main/java/com/example/supermonsterz/livingThings.java
+++ src/main/java/com/example/supermonsterz/livingThings.java
@@ -1,13 +1,20 @@
 package com.example.supermonsterz;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 public class livingThings {
     final String name;
     int speed;
     int health;
 
-    public livingThings(String name, int health){
+    public livingThings(String name, int health, Color color, int size){
         this.name = name;
         this.health = health;
+
+        Circle circle = new Circle(size);
+        circle.setFill(color);
+        circle.setStroke(color.darker());
     }
 
     public livingThings(String name, int health, int speed){
