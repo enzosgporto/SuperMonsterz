b8e2a0e75b9e4023ecd10387e6983e0ac901542b
enzosgporto <enzosgporto@hotmail.com>
2023-06-16 10:44:06 -0500

    Creates player object when user completes player creation

--- src/main/java/com/example/supermonsterz/ProfileController.java
+++ src/main/java/com/example/supermonsterz/ProfileController.java
@@ -1,8 +1,19 @@
 package com.example.supermonsterz;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ProfileController {
 
+    public Button playButton;
+    public ColorPicker colorSelector;
+    public TextField nameField;
+
+    public void playAction(ActionEvent actionEvent) {
+        SuperMonsterzApplication.Player = new player(nameField.getText(), 100, colorSelector.getValue());
+    }
 }
