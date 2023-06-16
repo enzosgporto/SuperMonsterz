32adeacf26885efdf98fd166eb54c8aa96bd4377
enzosgporto <enzosgporto@hotmail.com>
2023-06-16 10:44:30 -0500

    Added method to switch between scenes

--- src/main/java/com/example/supermonsterz/SuperMonsterzApplication.java
+++ src/main/java/com/example/supermonsterz/SuperMonsterzApplication.java
@@ -8,13 +8,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class SuperMonsterzApplication extends Application {
+    static public player Player;
+    private static Scene scene;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SuperMonsterzApplication.class.getResource("profile-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("SuperMonsterz");
         stage.setScene(scene);
         stage.show();
+    }
+
+    static void setScene(String controllerName) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(SuperMonsterzApplication.class.getResource(controllerName));
+        scene.setRoot(fxmlLoader.load());
     }
 
     public static void main(String[] args) {
